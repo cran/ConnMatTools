1dcd22c35b4bb3a95aa25f91a953b73a0307d9aa
David M. Kaplan <dmkaplan2000@gmail.com>
2013-07-09 00:00:00 +0000

    version 0.1.4

---
+++ R/ConnMatTools.R
@@ -0,0 +1,29 @@
+#' Tools for working with connectivity matrices
+#' 
+#' ConnMatTools collects several different methods for analyzing and
+#' working with connectivity matrices in R.  Though primarily oriented
+#' towards marine larval dispersal, many of the methods are general
+#' and useful for terrestrial systems as well.
+#'
+#' \tabular{ll}{
+#' Package: \tab ConnMatTools\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1.4\cr
+#' Date: \tab 2013-07-09\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab no\cr
+#' }
+#'
+#' @author
+#' David M. Kaplan \email{dmkaplan2000@@gmail.com}
+#' Marco Andrello \email{marco.andrello@@gmail.com}
+#' 
+#' Maintainer: David M. Kaplan \email{dmkaplan2000@@gmail.com}
+#' @name ConnMatTools
+#' @docType package
+#' @title Tools for working with connectivity matrices
+#' @keywords package
+#' @examples
+#' \dontrun{optimalSplitConnMat(CM)}
+#' @seealso See \code{\link{optimalSplitConnMat}}
+NULL
